7af61a1e84dd9e39676cd5186b2d20131a7d7f6e
tom tkeogh <thk11@aber.ac.uk>
2015-03-16 13:27:55 +0000

    Start
    Uplaod everything

---
+++ app/src/main/java/com/example/walkingtour/Help.java
@@ -0,0 +1,129 @@
+package com.example.walkingtour;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class Help extends Activity {
+
+	private static double EARTH_CIRC_METERS = 40030218;
+	
+	ArrayList<pointof> distances = new ArrayList<pointof>();
+	
+	String help;
+
+
+	private Location location;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_help);
+		TextView text = (TextView) findViewById(R.id.viewer);
+
+		LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		location = locationManager
+				.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+		double lat = location.getLatitude();
+		double lon = location.getLongitude();
+
+		
+		pointof PS = new pointof("Physical Sciences",distanceTo(lat, lon, 52.4159519,-4.0654581));
+		distances.add(PS);
+		
+		pointof HO = new pointof("Hugh Owen",distanceTo(lat, lon, 52.4157893,-4.063691));
+		distances.add(HO);
+		
+		pointof TP = new pointof("Thomas Parry",distanceTo(lat, lon, 52.4096535,-4.0521135));
+		distances.add(TP);
+		
+		pointof shortest = sortDist(distances);
+		
+		if (shortest.getName().equals("Physical Sciences")){
+			
+			
+			help = "Your closest point of help is the Physical Sciences Library. \n "
+					+ "Person of Contact : Amelia Lees \n "
+					+ "Number : (01970)62 2407 ";
+			
+		}
+		
+		if (shortest.getName().equals("Hugh Owen")){
+			
+			help = "Your closest point of help is the Hugh Owen Building \n"
+					+ "Person of Contact : Rhiannon Owen \n"
+					+ "Number : (01970) 62 2400 ";
+			
+		}
+		
+		if (shortest.getName().equals("Thomas Parry")){
+			
+			help = "Your closest point of help is the Thomas Parry building \n"
+					+ "Person of Contact : Amelia Lees \n"
+					+ "Number : (01970) 62 2411 ";
+			
+		}
+		
+		
+		
+		
+		text.setText(help);
+
+	}
+
+	private pointof sortDist(ArrayList<pointof> newdist) {
+		int index = 0;
+		
+		pointof small = newdist.get(0);
+		
+		for (int i =0;i<newdist.size();i++){
+			
+			if(newdist.get(i).getDist() < small.getDist()){
+				
+				index = i;
+			}
+			
+			
+		}
+		
+		
+		return newdist.get(index);
+		
+		
+		
+	}
+
+	private double distanceTo(double lat1, double lon1, double lat2, double lon2) {
+		double theta = lon1 - lon2;
+		double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2))
+				+ Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2))
+				* Math.cos(deg2rad(theta));
+		dist = Math.acos(dist);
+		dist = rad2deg(dist);
+		dist = dist * 60 * 1.1515;
+		dist = dist * 1.609344;  //to KM
+		return dist;
+	}
+
+	private double rad2deg(double rad) {
+		return (rad * 180.0 / Math.PI);
+	}
+	
+	private double deg2rad(double deg) {
+	      return (deg * Math.PI / 180.0);
+	    }
+	
+	
+	
+	
+}
---
+++ app/src/main/java/com/example/walkingtour/locations.java
@@ -0,0 +1,146 @@
+package com.example.walkingtour;
+
+import java.util.ArrayList;
+
+public class locations {
+	
+	private String name;
+	private String des;
+	private String type;
+	private double lat;
+	private double lon;
+	
+	
+	
+	public locations(String string, String string2,String type, double i, double j) {
+		
+		this.name = string;
+		this.des = string2;
+		this.type = type;
+		this.lat = i;
+		this.lon = j;
+	
+	}
+
+
+
+	public locations() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+	public ArrayList<locations> populate() {
+		
+		ArrayList<locations> info = new ArrayList<locations>();
+		
+		locations a = new locations("Edward Lloyd","Lecture Theatres","Lecture",52.4156455,-4.0664103);
+		info.add(a);
+		locations b = new locations("IBERS","Lecture Theatres","Lecture",52.4163857,-4.0668202);
+		info.add(b);
+		locations c = new locations("School Of Business and Management","Lecture Theatres","Lecture",52.4171251,-4.0672997);
+		info.add(c);
+		locations d = new locations("Pantacelyn","Welsh Speaking Accomodation","Accomodation",52.4164723,-4.0693913);
+		info.add(d);
+		locations e = new locations("Institute of Maths and Physics","Lecture Theatres","Lecture",52.4159151,-4.0655077);
+		info.add(e);
+		locations f = new locations("Llandinam","Lecture Theatres","Lecture",52.4164982,-4.0663893);
+		info.add(f);
+		locations g = new locations("Students Union","Home of SU","Facilities",52.4151517,-4.0630464);
+		info.add(g);
+		locations h = new locations("Arts Centre","Contains Great Hall","Facilities",52.4156345,-4.0628907);
+		info.add(h);
+		locations i = new locations("Hugh Owen Library","Books","Facilties",52.4158165,-4.0636652);
+		info.add(i);
+		locations j = new locations("Hugh Owen Building","Lecture Theatre","Lecture",52.4166614,-4.0641835);
+		info.add(j);
+		locations k = new locations("Department of Theatre","Also Film/Drama","Lecture",52.416998,-4.0626962);
+		info.add(k);
+		locations l = new locations("Sports Centre","Gym etc","Facilties",52.4143772,-4.0662023);
+		info.add(l);
+		locations m = new locations("National Library","More Books","Facilities",52.4143308,-4.067746);
+		info.add(m);
+		locations n = new locations("Cwrt Mawr","Blocks","Accomodation",52.4169516,-4.061601);
+		info.add(n);
+		locations o = new locations("Rosser Hall","Blocks","Accomodation",52.417708,-4.0609177);
+		info.add(o);
+		locations p = new locations("Llanbadarn","Lecture place","Lecture",52.4103625,-4.0521911);
+		info.add(p);
+		locations q = new locations("PJM","Blocks","Accomodation",52.4202346,-4.062425);
+		info.add(q);
+		locations r = new locations("Train Station","Trains","Facilities",52.4140782,-4.0818546);
+		info.add(r);
+		
+		
+		
+		return info;
+		
+		
+		
+		
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getDes() {
+		return des;
+	}
+
+
+
+	public void setDes(String des) {
+		this.des = des;
+	}
+
+
+
+	public String getType() {
+		return type;
+	}
+
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+
+	public double getLat() {
+		return lat;
+	}
+
+
+
+	public void setLat(double lat) {
+		this.lat = lat;
+	}
+
+
+
+	public double getLon() {
+		return lon;
+	}
+
+
+
+	public void setLon(double lon) {
+		this.lon = lon;
+	}
+	
+	
+	
+
+}
